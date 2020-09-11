f8ebb545ab69bb695785aed8b1e9af66018c62b2
Adam M. Wilson <adammichaelwilson@gmail.com>
2020-09-11 11:58:49 -0400

    update file name

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 
 # the next line downloads the most recent copy of the test file
-download.file("https://raw.githubusercontent.com/AdamWilsonLabEDU/geo511_tasks_tests/master/tests.R","tests/testthat/test-case_studies_downloaded.R")
+download.file("https://raw.githubusercontent.com/AdamWilsonLabEDU/geo511_tasks_tests/master/tests.R","tests/testthat/test-case_studies.R")
 
 # run all tests in the testhat folder
 test_dir("tests/testthat/")
