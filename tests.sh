c3676d793339e632170531933b4ec96552056f1b
Adam M. Wilson <adamw@buffalo.edu>
2021-09-14 13:56:37 -0400

    add autograding

---
+++ tests.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# this file is used to run the tests from github classroom / github actions.  See the .github/classroom/autograding.json file for details.
+# Could potentially break the testthat.R into separate tests and assign different points for each test. 
+
+echo "Running tests..."
+
+Rscript -e "questionr::qscan(list.files(pattern="*.R",recursive=T), load = TRUE, detail = FALSE)"
+Rscript -e "testthat::test_dir(testthat::test_path(),stop_on_failure = TRUE,reporter = 'progress')"
+
+ 
+# 
+# if Rscript -e "source('tests/testthat.R')" ; then
+#  echo "Pass: Program exited zero"
+# else
+#  echo "Fail: Program did not exit zero"
+#  exit 1
+# fi
+# 
+# echo "All tests passed."
+# 
+# exit 0
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,7 @@
-library(testthat)
-
-# the next line downloads the most recent copy of the test file
-if(!file.exists("tests/testthat/")) dir.create("tests/testthat/")
-
 #repo="https://raw.githubusercontent.com/AdamWilsonLabEDU/geo511_tasks_tests"
 
-#download.file(url = paste0(repo,"/master/tests.R"),
- #             destfile="tests/testthat/test-case_studies.R")
+questionr::qscan(list.files(pattern="*.R",recursive=T), load = TRUE, detail = FALSE)
 
 setwd(devtools::package_file())
 
-# run all tests in the testhat folder
-test_dir(test_path(),stop_on_failure = FALSE,reporter = "progress")
+testthat::test_dir(testthat::test_path(),stop_on_failure = TRUE,reporter = "progress")
